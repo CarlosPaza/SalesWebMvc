58e5d1347c087033c93743f7f4dfbcbae6639824
Carlos César Paza <carlos_paza@hotmail.com>
2020-08-02 12:25:51 -0300

    Update do vendedor

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -47,5 +47,23 @@ namespace SalesWebMvc.Services
                 throw new IntegrityException("Can't delete seller because he/she has sales");
             }
         }
+
+        public void Update(Seller obj)
+        {
+            bool hasAny = _context.Seller.Any(x => x.Id == obj.Id);
+            if (!hasAny)
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
     }
 }
